8eed52f4efb9232f2adca41b5e3bfc83e8ed5de3
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2018-10-10 16:59:39 +0900

    For weko community debug.

--- invenio_communities/forms.py
+++ invenio_communities/forms.py
@@ -40,7 +40,7 @@ class CommunityForm(Form):
     field_sets = [
         ('Information',
          ['identifier', 'title', 'description', 'curation_policy', 'page',
-          'community_header', 'logo', ],
+          'community_header', 'community_footer', 'logo', ],
          {'classes': 'in'}),
     ]
 
@@ -139,11 +139,18 @@ class CommunityForm(Form):
             ' which will be displayed on the index page of the community.'),
     )
 
+    community_footer = TextAreaField(
+        description=_(
+            'Optional. A footer design of the community collection,'
+            ' which will be displayed on the index page of the community.'),
+    )
+
     field_icons = {
         'identifier': 'barcode',
         'title': 'file-alt',
         'description': 'pencil',
         'community_header': 'pencil',
+        'community_footer': 'pencil',
         'curation_policy': 'check',
     }
 
