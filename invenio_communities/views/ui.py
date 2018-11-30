e1c124fb0a5444ac85dba256aec31764d43e07a3
weko3-dev13 <weko3-hitachi-dev13@ml.itg.hitachi.co.jp>
2018-11-30 11:23:07 +0900

    fixup

--- invenio_communities/views/ui.py
+++ invenio_communities/views/ui.py
@@ -144,13 +144,13 @@ def view(community):
         return generic_item(
             community, current_app.config['COMMUNITIES_DETAIL_TEMPLATE'])
 
+    current_app.logger.debug("AAAA")
     ctx = {'community': community}
     community_id = community.id
     # Get index style
     style = IndexStyle.get(current_app.config['WEKO_INDEX_TREE_STYLE_OPTIONS']['id'])
     width = style.width if style else '3'
 
-    current_app.logger.debug("aaaa")
     return render_template(
         current_app.config['COMMUNITIES_CURATE_TEMPLATE'],
         community_id=community_id, width=width, **ctx
